36df5dc2f219f31af781e01147a9795bacadfd84
Maazul Haque <hmaazul25@gmail.com>
2023-09-01 10:13:22 +0530

    input output added

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 var inputYear = document.querySelector("#input-year");
 var btnCheck = document.querySelector("#btn-check");
 var outputLeapYear = document.querySelector("#output");
+
+// console.log(inputYear);
+btnCheck.addEventListener("click", clickHandeler);
+
+var num = inputYear.value;
+function clickHandeler(num) {
+    if( (num%400 === 0) || (num%100 != 0) & (num%4 === 0))
+    {
+        outputLeapYear.innerText = "Yes it's a leap year";
+    }
+    else{
+        outputLeapYear.innerText = "No it's not a leap year";
+    }
+}
